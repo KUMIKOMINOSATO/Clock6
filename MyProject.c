758dba2f878f858d056680b31f7be3d4cbcd3226
Shigemaru Nishiyama <SHIGEMAL@AOL.COM>
2015-09-18 21:39:04 +0900

    Revert MyProject.c
    Code has been reverted optimizing works

--- MyProject.c
+++ MyProject.c
@@ -7,23 +7,20 @@
 // Ver9 Delay logic may cause lost interrupt
 // 2015.Aug.26 Ver10.0 Shigemal (Comment update)
 // 12MHz crystal has been used on this clock, so 1 machine cycle is 1us
-// Ver12 Timer0 used by mode2 8bit (250us)
-//  Timer1 used by mode1 16bit 4ms sampling LED scanning
-//  Sampling continuous right timer250us bit4 by 4ms sampling.
-// It makes accurate 8ms timing with double sample.
-// Ver13 Tiemr0 250us Timer1 2.5ms for oversamplng on 8ms timing.
 //
 // Target board definition
 #define AKIT112
 //#define SHE879
 //
 // System Parameter
-#define TIM0_TICK 250 // 250uS
-#define VALTH0 ((0-TIM0_TICK) & 0xff ) // mode2 makes 8bit
 #define LED_SCAN 2500 // 2.5mS
-#define TICK_BASE 8000 // 8mS
+#define TICK_BASE 25000  // 25mS
 #define TIME1SEC 1000000 // 1Sec
-#define TICK_CNT ((TIME1SEC/TICK_BASE)-1) //
+#define TICK_CNT TIME1SEC/TICK_BASE //
+#define VALTL0 ((0-TICK_BASE)& 0xff) //
+#define OPT  3   //
+#define VALTL0OPT (VALTL0+OPT) //
+#define VALTH0 ((0-TICK_BASE)>> 8) //
 #define VALTL1 ((0-LED_SCAN) & 0xff) //
 #define VALTH1 ((0-LED_SCAN)>> 8) //
 #define PRESS_TIME 320000
@@ -37,7 +34,7 @@
  sbit dig_ctrl_2 at P3.B3;
  sbit dig_ctrl_1 at P3.B5;
   // 7seg data has been configured for SH-E879
- char digi_val[11]={0x08,0x3d,0x44,0x24,0x31,0x22,0x02,0x3c,0x00,0x20,0xff}; 
+ char digi_val[11]={0x08,0x3d,0x44,0x24,0x31,0x22,0x02,0x3c,0x00,0x20,0xff}; // SHE879
 #endif
 #ifdef AKIT112
  sbit dig_ctrl_6 at P3.B5;        //Declaring control pins of the seven segments
@@ -47,32 +44,36 @@
  sbit dig_ctrl_2 at P3.B1;
  sbit dig_ctrl_1 at P3.B0;
   // 7seg data has been configured for AKIT112
- const char digi_val[11]={0x40,0x79,0x24,0x30,0x19,0x12,0x02,0x78,0x00,0x10,0xff};
+ const char digi_val[11]={0x40,0x79,0x24,0x30,0x19,0x12,0x02,0x78,0x00,0x10,0xff}; // SH-E879
  #endif
  sbit key at P3.B7;       // 0 = keypress
   unsigned char blink = 0;
-  sbit blinkc at blink.B6;  // blink period is 64times of tick base
- unsigned char timer250us=0;   // 250us timer by Timer0
- unsigned char timer8ms=0;
+  sbit blinkc at blink.B4;  // blink period is 16times of tick base
  unsigned char dig_disp=0;
- unsigned char hour=0;
- unsigned char hour2=0;
- unsigned char hour1=0;
- unsigned char min2=0;
- unsigned char min1=0;
- unsigned char sec2=0;
- unsigned char sec1=0;
+ int hour=0;
+ int hour2=0;
+ int hour1=0;
+ int min2=0;
+ int min1=0;
+ int sec2=0;
+ int sec1=0;
  unsigned char tempo =0;
  unsigned char mode=0;     // 0: online, 1: idle power, 2: min set, 3: hour set.
- unsigned char postflg=0;
- unsigned char timer_1_cnt=0;
+ int postflg=0;
+ int timer_0_cnt=0;
 
 
 
  const char hour_1[24]={0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3};
  const char hour_2[24]={0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,2,2,2,2};
 
-
+// Tick Interrupt create for 25ms
+void timer0_int() iv IVT_ADDR_ET0 ilevel 2 ics ICS_AUTO {
+    TL0=TL0+VALTL0OPT;
+    TH0=VALTH0;
+    TR0_bit=1; // It may not necessary
+    timer_0_cnt++;    // post interrupt flag to main
+}
 void delay()        //Function to provide a time delay of approx. 1 second. using Timer 1.
 {
 // This logic has to confirm timer_0 tick check and if not make idle force.
@@ -81,10 +82,12 @@ void delay()        //Function to provide a time delay of approx. 1 second. usin
 // so following delay fucntion timing may capture happened interupt later.
 // This logic should happened all of 25ms interrupt without lost.
 // and timer 0 interrupt should maintain 25ms interval as accurate.
-    while(timer_1_cnt==0 ){   // wait post flag from interrupt
+    while(timer_0_cnt==0 ){   // wait post flag from interrupt
         // PCON.B0 = 1;       // idle mode request for avoid timer1 (dynamic scan interrupt)
     };
-    --timer_1_cnt;    // clear post flag (25ms)
+    EA_bit=0;
+    timer_0_cnt--;    // clear post flag (25ms)
+    IE=0x8a;
 }
 /*
  unsigned char keycnt=0;
@@ -114,28 +117,15 @@ unsigned char keyscan()
      }
 }
 
-void timer0() iv IVT_ADDR_ET0 ilevel 0 ics ICS_AUTO {
-     ++timer250us;
-}
-
-// 4ms pseudo interval timer1 by calibration
-
-void timer1() iv IVT_ADDR_ET1 ics ICS_AUTO {
+void display() iv IVT_ADDR_ET1 ilevel 4 ics ICS_AUTO {
 //Function to display the number using seven segmnet multiplexing. For more details refer seven segment multiplexing.
 
-    TL1=VALTL1;        //Reloading Timer1
+    TL1=VALTL1;        //Reloading Timer0
     TH1=VALTH1;
-    if(timer250us.B4==4){
-        if(timer8ms==0){
-            timer8ms=1;
-            ++timer_1_cnt;
-        }
-    }else{
-          timer8ms =0;
-    }
-    P3=0xbf;
+    P3=0xff;
     P1=0xff;
-    dig_disp=++dig_disp%6;
+    dig_disp++;
+    dig_disp=dig_disp%6;
     switch (mode){         // No display
     case 0:
         switch(dig_disp){
@@ -158,18 +148,18 @@ void timer1() iv IVT_ADDR_ET1 ics ICS_AUTO {
         P1=    digi_val[min2];
         dig_ctrl_4 = 0;
         break;
-
+        
         case 4:
         P1=    digi_val[hour1];
         dig_ctrl_5 = 0;
         break;
-
+        
         case 5:
         P1=    digi_val[hour2];
         dig_ctrl_6 = 0;
         break;
         }
-
+        break;
     case 2:    // Min set mode blinking
         switch(dig_disp){
         case 0:
@@ -234,7 +224,6 @@ void timer1() iv IVT_ADDR_ET1 ics ICS_AUTO {
              P1=    digi_val[hour2];
              if (blinkc ==1) dig_ctrl_6 = 0;
              break;
-
         }
         break;
     }
@@ -246,22 +235,22 @@ void timer1() iv IVT_ADDR_ET1 ics ICS_AUTO {
 void main()
 {
     IE=0;     // All interrupt disable
-    PCON=0;
-    SCON=0;
-    TMOD=0x12;        //Intialize Timer1=mode1 Timer0=2
-    TL0=VALTH0;       // Timer0 by mode 2 with 250us interval
-    TH0=VALTH0;
+    TMOD=0x11;        //Intialize Timer 1
     TL1=VALTL1;
-    TH1=VALTH1;         // - 4000 (4ms)
-    TCON = 0x50;      // TR1 TR0 count enable
-    IP = 0x08; //   PT0_Bit = 1;      // high priority Timer 1  (LED Scan)
-    IE=0x8A;        // Enable Timer 1 and 0 interrupt
+    TH1=VALTH1;         // - 2500 (2.5ms)
+    TR1_bit=1;        //Start Timer 1
+    TL0=VALTL0;         // - 25000 ( 25ms )
+    TH0=VALTH0;
+    TR0_bit=1;      // Start Timer 0 (2.5ms)
+    PT1_Bit = 0;      // low priority Timer 1 (LED Scan)
+    PT0_Bit = 1;      // high priority Timer 0 (2.5ms)
+    IE=0x8A;        // Enable Timer 1 & 0 interrupt
     while(1)        //Start clock
     {
           //Debug code for display position
           //hour=0;hour2=1;hour1=2;min2=3;min1=4;sec2=5;sec1=6;
           //while(1);
-          delay();    // wait interrupt 4mS
+          delay();
           switch(mode) {
           case 0: // Online;
           case 1:
@@ -278,8 +267,8 @@ void main()
                                            if (++hour>23) {
                                                  hour = 0;
                                            }
-                                           hour2 = hour / 10;
-                                           hour1 = hour % 10;
+                                           hour2 = hour_2[hour];
+                                           hour1 = hour_1[hour];
                                      }
                                }
                          }
@@ -329,8 +318,8 @@ void main()
                     if (++hour>23) {
                           hour = 0;
                     }
-                    hour2 = hour / 10;
-                    hour1 = hour % 10;
+                    hour2 = hour_2[hour];
+                    hour1 = hour_1[hour];
                     break;
               case  2: // Press
                     mode = 0;  // move to hourset;
